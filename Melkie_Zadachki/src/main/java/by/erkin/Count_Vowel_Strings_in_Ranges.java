1077d49d045beb8b921346afeb2d9e4f646506cd
Erkin Kubatov <qoobatov@gmail.com>
2025-01-03 08:56:14 +0600

    new version count vowel strings

--- Melkie_Zadachki/src/main/java/by/erkin/Count_Vowel_Strings_in_Ranges.java
+++ Melkie_Zadachki/src/main/java/by/erkin/Count_Vowel_Strings_in_Ranges.java
@@ -1,31 +1,41 @@
 package by.erkin;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 public class Count_Vowel_Strings_in_Ranges {
 
-    private static boolean isVowel(char c) {
-        return "aeiou".indexOf(c) != -1;
+
+    public int[] vowelStrings(String[] words, int[][] queries) {
+        int[] ans = new int[queries.length];
+        HashSet<Character> vowels = new HashSet<>(
+                Arrays.asList('a', 'e', 'i', 'o', 'u')
+        );
+        int[] prefixSum = new int[words.length];
+        int sum = 0;
+        for (int i = 0; i < words.length; i++) {
+            String currentWord = words[i];
+            if (
+                    vowels.contains(currentWord.charAt(0)) &&
+                            vowels.contains(currentWord.charAt(currentWord.length() - 1))
+            ) {
+                sum++;
+            }
+            prefixSum[i] = sum;
+        }
+
+        for (int i = 0; i < queries.length; i++) {
+            int[] currentQuery = queries[i];
+            ans[i] = prefixSum[currentQuery[1]] - (currentQuery[0] == 0 ? 0 : prefixSum[currentQuery[0] - 1]);
+        }
+
+        return ans;
     }
 
     public static void main(String[] args) {
+        Count_Vowel_Strings_in_Ranges a = new Count_Vowel_Strings_in_Ranges();
         String[] words = {"aba", "bcb", "ece", "aa", "e"};
-        int[][] ranges = {{0, 2}, {1, 4}, {1, 1}};
-
-        for (int i = 0; i < ranges.length; i++) {
-            int start = ranges[i][0];
-            int end = ranges[i][1];
-            int count = 0;
-
-            for (int k = start; k <= end; k++) {
-                String word = words[k];
-                if (isVowel(word.charAt(0)) && isVowel(word.charAt(word.length() - 1))) {
-                    count++;
-                }
-            }
-
-            System.out.println("Query " + i + ": " + count);
-        }
-
-
-
+        int[][] queries = {{0, 2}, {1, 4}, {1, 1}};
+        a.vowelStrings(words, queries);
     }
 }
