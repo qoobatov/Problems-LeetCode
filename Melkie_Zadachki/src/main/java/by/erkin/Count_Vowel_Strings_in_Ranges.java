c830a887825989633c7bb0f78839160c1ac4f7d4
Erkin Kubatov <qoobatov@gmail.com>
2025-01-03 09:04:22 +0600

    new version of problem count vowel strings in ranges

--- Melkie_Zadachki/src/main/java/by/erkin/Count_Vowel_Strings_in_Ranges.java
+++ Melkie_Zadachki/src/main/java/by/erkin/Count_Vowel_Strings_in_Ranges.java
@@ -23,8 +23,6 @@ public class Count_Vowel_Strings_in_Ranges {
             }
             prefixSum[i] = sum;
         }
-
-
         for (int i = 0; i < queries.length; i++) {
             int[] currentQuery = queries[i];
             ans[i] = prefixSum[currentQuery[1]] - (currentQuery[0] == 0 ? 0 : prefixSum[currentQuery[0] - 1]);
