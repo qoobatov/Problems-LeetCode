77bbed8400e403a2734c2c47c8ff8dd04058018d
Erkin Kubatov <qoobatov@gmail.com>
2025-01-09 21:15:15 +0600

    correct version Roman to integer

--- Melkie_Zadachki/src/main/java/by/erkin/Roman_to_Integer.java
+++ Melkie_Zadachki/src/main/java/by/erkin/Roman_to_Integer.java
@@ -2,33 +2,40 @@ package by.erkin;
 
 public class Roman_to_Integer {
     public int romanToInt(String s) {
+        int result = 0;
+
         for (int i = 0; i < s.length(); i++) {
-            switch (s.charAt(i)) {
-                case 'I':
-                    return 1;
-                case 'V':
-                    return 5;
-                case 'X':
-                    return 10;
-                case 'L':
-                    return 50;
-                case 'C':
-                    return 100;
-                case 'D':
-                    return 500;
-                case 'M':
-                    return 1000;
-                default:
-                    break;
+            int currentValue = getValue(s.charAt(i));
+            // Если текущая цифра меньше следующей, вычитаем её, иначе добавляем
+            if (i + 1 < s.length() && currentValue < getValue(s.charAt(i + 1))) {
+                result -= currentValue;
+            } else {
+                result += currentValue;
             }
         }
-        return 0;
+
+        return result;
+    }
+
+    private int getValue(char roman) {
+        switch (roman) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: throw new IllegalArgumentException("Invalid Roman numeral");
+        }
     }
 
     public static void main(String[] args) {
         Roman_to_Integer romanToInteger = new Roman_to_Integer();
-        System.out.println(romanToInteger.romanToInt("III"));
+        System.out.println(romanToInteger.romanToInt("III"));   // 3
+        System.out.println(romanToInteger.romanToInt("IV"));    // 4
+        System.out.println(romanToInteger.romanToInt("IX"));    // 9
+        System.out.println(romanToInteger.romanToInt("LVIII")); // 58
+        System.out.println(romanToInteger.romanToInt("MCMXCIV"));// 1994
     }
-
 }
-
