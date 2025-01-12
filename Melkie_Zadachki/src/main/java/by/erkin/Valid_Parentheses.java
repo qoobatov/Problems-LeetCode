0fb8752cd19ab3a171d3adb8a194f1fdd103aa43
Erkin Kubatov <qoobatov@gmail.com>
2025-01-12 21:21:30 +0600

    Valid_Parentheses

---
+++ Melkie_Zadachki/src/main/java/by/erkin/Valid_Parentheses.java
@@ -0,0 +1,27 @@
+package by.erkin;
+
+import java.util.Stack;
+
+public class Valid_Parentheses {
+
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else if (c == ')' || c == '}' || c == ']') {
+                if (stack.isEmpty() || !isMatchingPair(stack.pop(), c) ) {
+                    return false;
+                }
+            }
+
+        }
+        return stack.isEmpty();
+    }
+
+    private boolean isMatchingPair(char open, char close) {
+        return open == '(' && close == ')' || open == '{' && close == '}' || open == '[' && close == ']';
+    }
+
+}
